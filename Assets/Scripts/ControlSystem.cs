d20af9e265aa34dc7c861b8d789000ec77ca69da
KID <50666207+KID421@users.noreply.github.com>
2023-05-25 20:01:17 +0800

    Update 添加控制系統移動

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -2,21 +2,38 @@
 
 public class ControlSystem : MonoBehaviour
 {
+    [Header("移動速度")]
+    [Range(0, 100)]
+    public float moveSpeed = 3.5f;
+    [Header("剛體")]
+    public Rigidbody2D rig;
+
     private void Awake()
     {
-        print(666);
-        print("嗨~");
-        print("1 + 2");
-        print($"{1 + 2}");
+        // print(666);
+        // print("嗨~");
+        // print("1 + 2");
+        // print($"{1 + 2}");
     }
 
     private void Start()
     {
-        print("<color=yellow>開始事件</color>");
+        // print("<color=yellow>開始事件</color>");
     }
 
     private void Update()
     {
-        print("<color=#998877>更新事件</color>");
+        // print("<color=#998877>更新事件</color>");
+
+        // 呼叫移動方法
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rig.velocity = new Vector2(h, v) * moveSpeed;
     }
 }
