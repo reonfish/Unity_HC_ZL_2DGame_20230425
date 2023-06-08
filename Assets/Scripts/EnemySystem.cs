ff62df7e0deee1a42f84924083067249018ad3c2
KID <50666207+KID421@users.noreply.github.com>
2023-06-08 21:08:48 +0800

    Update 敵人移動

---
+++ Assets/Scripts/EnemySystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EnemySystem : MonoBehaviour
+{
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("爆走企鵝").transform;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, 0.01f);
+    }
+}
