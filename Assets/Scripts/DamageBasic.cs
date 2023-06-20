649d0c2b9952abe971f009efa2208a6dba7848f9
KID <50666207+KID421@users.noreply.github.com>
2023-06-20 21:34:52 +0800

    Update 無敵史萊姆

---
+++ Assets/Scripts/DamageBasic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DamageBasic : MonoBehaviour
+{
+    [Header("資料")]
+    public DataBasic data;
+
+    private float hp;
+
+    private void Awake()
+    {
+        hp = data.hp;
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+
+        print($"<color=#ffee66>{gameObject.name} 血量剩下：{hp}</color>");
+
+        if (hp <= 0) Dead();
+    }
+
+    private void Dead()
+    {
+        print($"<color=#ff9966>{gameObject.name} 死亡</color>");
+    }
+}
